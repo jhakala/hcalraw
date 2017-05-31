fdf098342e4fdb7595dec91f7cc2797b7bb362d3
John Hakala <john_hakala@brown.edu>
2017-05-31 14:44:17 -0500

    make a pared-down plugin that makes a ADC vs. TS histo

---
+++ plugins/example.py
@@ -0,0 +1,52 @@
+import collections
+from configuration import hw, sw
+import printer
+
+def example(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, **_):
+    # sanity check for incoming raw data
+    for i, raw in enumerate([raw1, raw2]):
+        if not raw:
+            continue
+
+        # find the number of time samples in the data
+        nTsMax = raw[None]["firstNTs"]
+        for fedId, d in sorted(raw.iteritems()):
+            if fedId is None:
+                continue
+
+        # get the important chunks of the raw data
+        blocks=d["htrBlocks"].values()
+
+        # sanity checks for these chunks
+        for block in blocks:
+            if type(block) is not dict:
+                printer.warning("FED %d block is not dict" % fedId)
+                continue
+            elif "channelData" not in block:
+                printer.warning("FED %d block has no channelData" % fedId)
+                continue
+                                                                         
+        # bookkeeping for error flags
+        ErrF = {}
+        for i in range(4):
+            ErrF[i] = 0
+
+
+            for channelData in block["channelData"].values():
+                if channelData["QIE"]:
+                    # check the error flags
+                    errf = "ErrFNZ" if channelData["ErrF"] else "ErrF0"
+
+                    # some logic for naming two histograms, one for a clean error flag and another with a problematic error flag
+                    eq = "!=" if channelData["ErrF"] else "=="
+
+                    nAdcMax = 256
+
+                    for (i, adc) in enumerate(channelData["QIE"]):
+                        if nTsMax <= i:
+                            break
+                        
+                        # fill a 2d histogram where bins on y-axis are ADCs and bins on x-axis are time slice index 
+                        book.fill((i, adc), "ADC_vs_TS_%s_%d" % (errf, fedId),
+                                  (nTsMax, nAdcMax), (-0.5, -0.5), (nTsMax - 0.5, nAdcMax - 0.5),
+                                  title="FED %d (ErrF %s 0);time slice;ADC;Counts / bin" % (fedId, eq))
